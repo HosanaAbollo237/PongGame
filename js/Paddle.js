0c81ecdf18e99c98fba4ca469eac30e6b6f32503
HosanaAbollo237 <hosana.abollo2377@gmail.com>
2020-11-05 17:18:35 +0100

    Mouvement des paddles

--- js/Paddle.js
+++ js/Paddle.js
@@ -1,14 +1,26 @@
 class Paddle{
-
+    // Constructor
     constructor(x){
         this.x = x;
         this.y = height / 2;
-        this.height = 70;
-        this.width = 10;
+        this.height = 80;
+        this.width = 20;
     }
     
+    // Permet d'afficher le paddle 
     display(){
         fill(255)
         rect(this.x, this.y, this.width, this.height)
     }
+
+    // Aim to move up and down
+    up(){
+        this.y -= 2
+    }
+
+    down(){
+        this.y += 2
+    }
+
+    
 }
--- js/sketch.js
+++ js/sketch.js
@@ -2,11 +2,16 @@
 function setup(){
     createCanvas(624, 351)
     playerPaddle = new Paddle(15)
+    aiPaddle = new Paddle(width - 37)
 }
 
 // Remplissage de couleur du canvas
 function draw(){
     background(0)
     playerPaddle.display()
+    aiPaddle.display()
+
+    playerPaddle.up()
 }
+
 
